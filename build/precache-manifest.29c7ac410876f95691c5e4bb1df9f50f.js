86fd0dc66cc183a5a96f36b0fc28a42a490f2ac8
Rohan Agarwal <rohanagarwal21u@gmail.com>
2020-10-31 12:02:01 +0530

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.29c7ac410876f95691c5e4bb1df9f50f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4205792b11e9e96ac75a2e017eec2c13",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "85c7fafbb16d3ac54fbc",
+    "url": "/react-gh-pages/static/js/2.674409cf.chunk.js"
+  },
+  {
+    "revision": "6252714ca30b20302621",
+    "url": "/react-gh-pages/static/js/main.d5aba2ac.chunk.js"
+  },
+  {
+    "revision": "94c4514f6443c252ae0f",
+    "url": "/react-gh-pages/static/js/runtime-main.d701a425.js"
+  },
+  {
+    "revision": "124ee832dcbbb0464c49712fab1ba5c0",
+    "url": "/react-gh-pages/static/media/Axelerate.124ee832.jpg"
+  },
+  {
+    "revision": "213d9b8939cc951a1a10a5e40f1f4d8e",
+    "url": "/react-gh-pages/static/media/aquadrive.213d9b89.jpg"
+  },
+  {
+    "revision": "8a8ad2b932abc6ad58829100563cb67a",
+    "url": "/react-gh-pages/static/media/atv-gallery.8a8ad2b9.png"
+  },
+  {
+    "revision": "8e6f58525ee34e7790299fed48af0d87",
+    "url": "/react-gh-pages/static/media/atv.8e6f5852.jpg"
+  },
+  {
+    "revision": "74331f50fb49c494905d3779a56c6010",
+    "url": "/react-gh-pages/static/media/axelerate20-1.74331f50.jpg"
+  },
+  {
+    "revision": "82a786281b375af9bc75bdec73836547",
+    "url": "/react-gh-pages/static/media/design.82a78628.jpg"
+  },
+  {
+    "revision": "8cbe39174a9d6ad905bb244250e4c591",
+    "url": "/react-gh-pages/static/media/harsh.8cbe3917.jpg"
+  },
+  {
+    "revision": "2cf54d5b14d7c1d9022ab20916bd6f37",
+    "url": "/react-gh-pages/static/media/lady-img.2cf54d5b.jpg"
+  },
+  {
+    "revision": "f3988e9a8ef7c7522d69cd205a7a9a14",
+    "url": "/react-gh-pages/static/media/quiz.f3988e9a.png"
+  },
+  {
+    "revision": "9ac313913fca54dbdee81b8682877f98",
+    "url": "/react-gh-pages/static/media/rc2.9ac31391.jpg"
+  },
+  {
+    "revision": "d51d00738222093bd376fe8ea7f8dad7",
+    "url": "/react-gh-pages/static/media/saelogohd2.d51d0073.png"
+  },
+  {
+    "revision": "a8af31f138ad562a32627231da4e211c",
+    "url": "/react-gh-pages/static/media/screws.a8af31f1.png"
+  },
+  {
+    "revision": "4bcc833ed8fa3ed84d7e73c0bf81790e",
+    "url": "/react-gh-pages/static/media/self-driving.4bcc833e.jpg"
+  },
+  {
+    "revision": "19cfa842b49cdd9cc7ebefb7b9ec407d",
+    "url": "/react-gh-pages/static/media/smartbike.19cfa842.jpg"
+  },
+  {
+    "revision": "f9a573dc6e099eb88081725b2da54440",
+    "url": "/react-gh-pages/static/media/sudeep.f9a573dc.jpg"
+  },
+  {
+    "revision": "fa8cb275a94ae0d594405d5baa69e71e",
+    "url": "/react-gh-pages/static/media/team.fa8cb275.png"
+  }
+]);
